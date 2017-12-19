9ff8a39dff71011ea49c10f9e9d0f05934b94a63
noonnopii <noonnopii@gmail.com>
2017-12-19 07:53:43 -0600

    using Thread.sleep for Ready-Set-Go

--- app/src/main/java/com/noonnopii/readysetgohornw/MainActivity.java
+++ app/src/main/java/com/noonnopii/readysetgohornw/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.media.MediaPlayer;
 import android.widget.TextView;
 
+import static java.lang.Thread.State.RUNNABLE;
+
 public class MainActivity extends WearableActivity {
 
     private int step = 0;
@@ -23,33 +25,36 @@ public class MainActivity extends WearableActivity {
         final MediaPlayer mp = MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                switch (step) {
-                    case 0:
-                       button.setText(getString(R.string.start_startButton));
-                       break;
-                    case 1:
-                        button.setText(getString(R.string.ready_ready));
-                        break;
-                    case 2:
-                        button.setText(getString(R.string.set_set));
-                        break;
-                    case 3:
-                        button.setText(getString(R.string.go_go));
-                        step = 0;
-                        try {
-                            if (mp.isPlaying()) {
-                                mp.stop();
-                                mp.release();
-                            } mp.start();
-                        } catch (Exception e) {e.printStackTrace();}
 
-                        break;
-                    default:
-                        button.setText(getString(R.string.start_startButton));
-                        break;
-                }
-                step++;
-                // this
+               button.setText(getString(R.string.start_startButton));
+               try{
+                   Thread.sleep(2000);
+                   Thread.start();
+                   try{
+                       Thread.sleep(2000);
+
+                       button.setText(getString(R.string.set_set));
+                       try{
+                           Thread.sleep(2000);
+                           button.setText(getString(R.string.go_go));
+                           try {
+                               if (mp.isPlaying()) {
+                                   mp.stop();
+                                   mp.release();
+                               } mp.start();
+                           } catch (Exception e) {e.printStackTrace();}
+                       }catch (InterruptedException e)
+                       {
+                           e.printStackTrace();
+                       }
+                   }catch (InterruptedException e)
+                   {
+                       e.printStackTrace();
+                   }
+               }catch (InterruptedException e)
+               {
+                   e.printStackTrace();
+               }
             }
         });
     }
